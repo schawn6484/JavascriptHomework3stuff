09cd34df31eebe0fb11b265c30f1466f17041767
schawn6484 <schawnetta.robinson@gmail.com>
2020-10-15 14:03:54 -0400

    added javascript screenshot to image folder

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Below are the screenshots:
 ![](images/Screen%20Shot%202020-10-14%20at%203.59.39%20PM.png)
 ![](images/Screen%20Shot%202020-10-14%20at%203.59.51%20PM.png)
 ![](images/Screen%20Shot%202020-10-14%20at%203.59.56%20PM.png)
+![](images/Screen%)
--- script.js
+++ script.js
@@ -10,5 +10,29 @@ function writePassword() {
 
 }
 
+//Function to generate password by length
+function generatePassword(){
+ 
+  var length = parseInt(prompt('how many characters would you like for your password to contain?'));
+  if(isNaN(length) === true){
+    alert('Numeric only')
+    return;
+  }
+  if(length < 8){
+    alert ('Password length > 8')
+    return
+  }
+  if(length > 128){
+    alert ('Password length < 128')
+    return
+  }
+
+  var hasSpecialChar =  confirm('Click Ok if you want special characters');
+  var hasUppercase =    confirm('Click Ok if you want Uppercase characters');
+  var hasLowercase =    confirm('Click Ok if you want Lowercase characters');
+
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
