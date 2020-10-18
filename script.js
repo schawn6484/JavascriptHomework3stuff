15b950f39372b677b91b3f741116e3929f577a86
schawn6484 <schawnetta.robinson@gmail.com>
2020-10-17 22:50:19 -0400

    added screenshots to image folder, comments and bootstrap to html, and generated password to javascript

--- script.js
+++ script.js
@@ -1,64 +1,120 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//Assigned code
+var generatePasswordButton = document.getElementById("generate");
+var copyToClipBoardButton = document.getElementById("copytoclipboard");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//Possible characters
+var onlyUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var onlyLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var onlySpecial = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','['];
+var onlyNumeric = [0,1,2,3,4,5,6,7,8,9];
 
-  passwordText.value = password;
+//Options
+function generateOptions() {
+     //prompt user for number restriction
+  var length = parseInt(prompt("How many characters would you like your password to contain?"));
+    if(isNaN(length) === true){
+      alert('Numeric only')
+      return;
+    }
+    //prompt user for length restriction
+    if (length < 8) {
+        alert('The password must be at least 8 characters!');
+        return;
+    }
+    if (length > 128) {
+        alert('The password must be less than 128 characters!');
+        return;
+    }
+
+    //prompt user for special character
+    var hasSpecialCharacter = confirm("Click Ok if you want a special character.");
+    
+    //prompt user for numeric character
+    var hasNumeric = confirm("Click Ok if you want a number.");
+    
+    //prompt user for lowercase character
+    var hasLowercase = confirm("Click Ok if you want a lowercase.");
+    
+    //prompt user for uppercase character
+    var hasUppercase = confirm("Click Ok if you want an uppercase.");
+    
+    if (!hasLowercase && !hasUppercase && !hasNumeric && !hasSpecialCharacter) {
+        alert("Your password must contain at least one special, numeric, lowercase, or uppercase character");
+        return
+    }
+  
+//Variable question options
+    var questionOptions = {
+        length: length,
+        specialCharacters: hasSpecialCharacter,
+        numeric: hasNumeric,
+        lowerCase: hasLowercase,
+        upperCase: hasUppercase
+    }
+
+    return questionOptions;
 
 }
+//Function generator password
+function generatePassword() {
 
-// Possible password values
-var SPECIALCHAR_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126))
-var UPPERCASE_CHAR_CODES =   arrayFromLowToHigh(65, 90)
-var LOWERCASE_CHAR_CODES =   arrayFromLowToHigh(97, 122)
-var NUMBER_CHAR_CODES =      arrayFromLowToHigh(48, 57)
+    var options = generateOptions();
+    console.log(options)
 
+    var passwordPool = [];
+    console.log(passwordPool)
 
-//Function to generate password by length
-//Length restrictions
-//Length is no less than 8 and no greater than 128
-function generatePassword(length, hasSpecialChar, hasUppercase, hasLowercase, hasNumber) {
-   let charCodes = LOWERCASE_CHAR_CODES
-   if (hasUppercase)charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-   if (hasSpecialChar)charCodes = charCodes.concat(SPECIALCHAR_CHAR_CODES)
-   if (hasNumber)charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+    if (options.specialCharacters) {
+        for (i = 0; i < onlySpecial.length; ++i) {
+            passwordPool.push(onlySpecial[i]);
+        }
+    } 
+    if (options.numeric) {
+        for (i = 0; i < onlyNumeric.length; ++i) {
+        passwordPool.push(onlyNumeric[i]);
+        }
+    }
+    if (options.lowerCase) {
+        for (i = 0; i < onlyLower.length; ++i) {
+        passwordPool.push(onlyLower[i]);
+        }
+    }
+    if (options.upperCase) {
+        for (i = 0; i < onlyUpper.length; ++i) {
+        passwordPool.push(onlyUpper[i]);
 
-  var length = parseInt(prompt('how many characters would you like for your password to contain?'));
-  if(isNaN(length) === true){
-    alert('Numeric only')
-    return;
-  }
-  if(length < 8){
-    alert ('Password length > 8')
-    return
-  }
-  if(length > 128){
-    alert ('Password length < 128')
-    return
-  }
-//Confirm Special, Uppercase, Lowercase, Number Characters
-  var hasSpecialChar =  confirm('Click Ok if you want special characters');
-  var hasUppercase =    confirm('Click Ok if you want Uppercase characters');
-  var hasLowercase =    confirm('Click Ok if you want Lowercase characters');
-  var hasNumber   =     confirm ('Click Ok if you want numbers');
+      }
+    }
+//Final password
+    var finalPassword = [];
 
+    for (let i = 0; i < options.length; ++i) {
+        var randomPicker = Math.floor(Math.random() * Math.floor(passwordPool.length));
+         finalPassword.push(passwordPool[randomPicker])
+    }
+
+    console.log(finalPassword)
+
+    var superFinal = finalPassword.join('');
+    console.log(superFinal)
+
+    document.getElementById("display").textContent = superFinal;
 }
 
-function arrayFromLowToHigh(low, high){
-  var array = []
-  for (let i = low; i <= high; i++){
-    array.push(i)
-  }
-  return array
+
+    var password = "";
+
+
+function copytoClipboard() {
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("Your password has now been copied to the clipboard");
+
 }
+//Password generated
+generatePasswordButton.addEventListener('click', generatePassword);
 
 
-//Conditional statements for Special Character, Uppercase, Lowercase
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
